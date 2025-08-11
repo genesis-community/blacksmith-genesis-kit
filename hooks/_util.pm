92e4c7781bc2abfcb2728a2b1c2be87e5025e04d
Wayne E. Seguin <wayneeseguin@gmail.com>
2025-08-11 19:08:26 -0400

    fix get_blacksmith_ip

--- hooks/_util.pm
+++ hooks/_util.pm
@@ -9,31 +9,21 @@
 sub _get_blacksmith_ip {
 	my ($self) = @_;
 	my $env = $self->env;
-	
+
 	# For OCFP deployments, get IP from vault
-	if ($self->want_feature('ocfp')) {
-		my $ocfp_env = $env->lookup('params.ocfp_env', '');
-		if ($ocfp_env) {
-			# Construct the vault path for OCFP reserved IPs
-			my $vault_path = "/ocf/net/subnets/ocfp-1/reserved-ips";
-			my $ip_key = "blacksmith_ip";
-			
-			# Try to get the IP from vault
-			my $vault_client = $self->vault;
-			if ($vault_client && $vault_client->exists($vault_path)) {
-				my $data = $vault_client->get($vault_path);
-				if ($data && $data->{$ip_key}) {
-					return $data->{$ip_key};
-				}
-			}
-			
-			# Fall back to params.ip if vault lookup fails
-			warning("Could not retrieve Blacksmith IP from OCFP vault path $vault_path:$ip_key, falling back to params.ip");
-		}
-	}
-	
-	# Default: get IP from params
-	return $env->lookup('params.ip', '');
+  if ($self->want_feature('ocfp')) {
+    # Construct the vault path for OCFP reserved IPs
+    my $vault_path = $env->secrets_base."ocf/net/subnets/ocfp-1/reserved-ips";
+    if ($env->vault->has($vault_path, "blacksmith_ip")) {
+      my $ip = $env->vault->get($vault_path, "blacksmith_ip");
+      return $ip;
+    } else {
+      # Fall back to params.ip if vault lookup fails
+      warning("Could not retrieve Blacksmith IP from OCFP vault path $vault_path:blacksmith_ip, falling back to params.ip");
+    }
+  }
+  # Default: get IP from params
+  return $env->lookup('params.ip', '');
 }
 
 1;
