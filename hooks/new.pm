6c3f4a920224fba047495b5caab63cd3901cea6f
Wayne E. Seguin <wayneeseguin@gmail.com>
2025-08-22 22:12:42 -0400

    h00ks are n3w!

---
+++ hooks/new.pm
@@ -0,0 +1,40 @@
+package Genesis::Hook::New::Blacksmith v3.3.0;
+
+use strict;
+use warnings;
+use v5.20; # Genesis supports min perl v5.20.
+
+# Parent class inheritance
+use parent qw(Genesis::Hook);
+
+# Import required functions
+use Genesis qw/trace bug bail warning info/;
+use Genesis::Term qw/prompt_for_boolean prompt_for_select prompt_for_line prompt_for prompt_for_block in_controlling_terminal/;
+use Genesis::UI qw/output/;
+use Data::Dumper;
+use File::Basename qw/basename dirname/;
+use File::Path qw/mkpath/;
+
+sub init {
+  my ($class, %ops) = @_;
+  my $obj = $class->SUPER::init(%ops);
+	$obj->{files} = [];
+	$obj->check_minimum_genesis_version('3.1.0');
+  $obj->{features} = [];
+  $obj->{config} = {
+    kit => {
+      name => $ENV{GENESIS_KIT_NAME},
+      version => $ENV{GENESIS_KIT_VERSION},
+      features => []
+    },
+    params => {}
+  };
+  return $obj;
+}
+
+sub perform {
+  return $self->done();
+}
+
+1;
+# vim: set ts=2 sw=2 sts=2 noet fdm=marker foldlevel=1:
